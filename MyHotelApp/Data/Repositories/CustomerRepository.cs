163a79407f1c013ea1066d31eb0a1a508f293264
Emma Lübke <lubke.emma@gmail.com>
2024-12-05 14:53:22 +0100

    Lagt till repositorys och customerservice

--- MyHotelApp/Data/Repositories/CustomerRepository.cs
+++ MyHotelApp/Data/Repositories/CustomerRepository.cs
@@ -4,9 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace MyHotelApp.Data.Repositories
 {
-    internal class CustomerRepository
+    public class CustomerRepository
     {
+        private readonly HotelDbContext _context;
+
+        public CustomerRepository(HotelDbContext context)
+        { 
+            _context = context;
+        }
+
+        public Customer GetCustomerById(int id)
+        {
+            return _context.Customers.First(c => c.Id == id);
+        }
+
+        public List<Customer> GetAllCustomers()
+        {
+            return _context.Customers.ToList();
+        }
     }
 }
