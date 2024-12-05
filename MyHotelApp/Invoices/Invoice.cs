d5cfc53f316a9f30c619a3063613065a3c4513ca
Emma Lübke <lubke.emma@gmail.com>
2024-12-05 16:05:01 +0100

    Lagt till en mapp och klass för fakturor

---
+++ MyHotelApp/Invoices/Invoice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHotelApp.Invoices
+{
+    public class Invoice
+    {
+        public int Id { get; set; }
+        public string BookingId { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime IssueDate { get; set; }
+        public bool IsPaid {  get; set; }
+        public bool IsCanceled { get; set; }
+    }
+}
